5fa6d59b5598abaf59195203ac3a85fb204d5cf8
David Gómez <dagove47@gmail.com>
2024-12-09 20:42:10 -0600

    Initial commit

---
+++ Practica_3_Web/Practica_3_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica_3_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
